053bd2c0c21567ea20d420ad97c5028d30395fd8
Rajesh-dev3 <rajesh@bellatorgroup.in>
2024-05-17 02:27:45 -0400

    removeExtraRoute

--- src/routes/PagesRoutes.jsx
+++ src/routes/PagesRoutes.jsx
@@ -11,10 +11,7 @@ export const router = createBrowserRouter([
           path: "/",
           element: <Home/>,
         },
-        {
-          path: "/",
-          element: <Home/>,
-        },
+   
       ],
     },
   ]);
